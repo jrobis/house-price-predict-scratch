968b76d7da7bcb4ba6fc977292587eefcd19f906
jarsushi <joshua.robison.a@gmail.com>
2020-06-21 09:07:29 -0600

    Created prediction form

--- src/api/controller.py
+++ src/api/controller.py
@@ -1,15 +1,17 @@
 from flask import request, Blueprint, jsonify, render_template
 
+from api.form_model import InputForm
+from xgboost_model.predict import make_prediction
 from xgboost_model import __version__ as _version
 from api import __version__ as api_version
 
 prediction_app = Blueprint('prediction_app', __name__)
 
 
-@prediction_app.route('/', methods=['GET'])
-def home():
-    if request.method == 'GET':
-        return render_template("home.html")
+# @prediction_app.route('/', methods=['GET'])
+# def home():
+#     if request.method == 'GET':
+#         return render_template("home.html")
 
 
 @prediction_app.route('/about', methods=['GET'])
@@ -32,7 +34,22 @@ def version():
                         'api_version': api_version})
 
 
-@prediction_app.route('/foo', methods=['GET'])
-def foo():
-    if request.method == 'GET':
-        return "You did it!"
+@prediction_app.route('/', methods=['GET', 'POST'])
+def index():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        data = {'title': form.title.data
+                                  , 'address': form.address.data
+                                  , 'city': form.city.data
+                                  , 'state': form.state.data
+                                  , 'postal_code': form.postal_code.data
+                                  , 'facts and features': form.facts_and_features.data
+                                  , 'real estate provider': form.real_estate_provider
+                                  , 'url': form.url}
+        result = make_prediction(data)
+    else:
+        result = None
+
+    return render_template('home.html', form=form, result=result)
+
