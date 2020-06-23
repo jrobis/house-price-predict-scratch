2b0830c2ee1c830bfe30a1eafaeb261c57cd0c13
jarsushi <joshua.robison.a@gmail.com>
2020-06-23 11:13:28 -0600

    Created dist

---
+++ src/xgboost_model/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xgboost-model-pkg-jarsushi", # Replace with your own username
+    version="0.1.0",
+    author="Joshua Robison",
+    author_email="joshua.robison.a@gmail.com",
+    description="A small xgboost model package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jarsushi/house-price-predict-scratch/tree/master/src/xgboost_model",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
