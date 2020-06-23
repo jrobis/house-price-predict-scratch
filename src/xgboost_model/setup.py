95109edb12709d1e88f546d133cafac99854c32f
jarsushi <joshua.robison.a@gmail.com>
2020-06-23 13:06:13 -0600

    Updated setup.py and dist

--- src/xgboost_model/setup.py
+++ src/xgboost_model/setup.py
@@ -1,11 +1,17 @@
+from pathlib import Path
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+NAME = "xgboost_model"
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+
 setuptools.setup(
     # name="xgboost-model-pkg-jarsushi", # Replace with your own username
-    name="xgboost_model",
+    name=NAME,
     version="0.1.0",
     author="Joshua Robison",
     author_email="joshua.robison.a@gmail.com",
