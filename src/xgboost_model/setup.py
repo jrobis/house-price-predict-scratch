b690169db8b504eb1a9d27a2fc3051d95807b8f7
jarsushi <joshua.robison.a@gmail.com>
2020-06-23 11:16:39 -0600

    Created dist_v2

--- src/xgboost_model/setup.py
+++ src/xgboost_model/setup.py
@@ -4,7 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xgboost-model-pkg-jarsushi", # Replace with your own username
+    # name="xgboost-model-pkg-jarsushi", # Replace with your own username
+    name="xgboost_model",
     version="0.1.0",
     author="Joshua Robison",
     author_email="joshua.robison.a@gmail.com",
