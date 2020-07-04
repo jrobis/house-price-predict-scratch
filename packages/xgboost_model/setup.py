9f058423e189adb08592218e70c8e98bd228c891
jarsushi <joshua.robison.a@gmail.com>
2020-07-04 14:37:29 -0600

    Fixed package by including all files under xgboost_model folder

--- packages/xgboost_model/setup.py
+++ packages/xgboost_model/setup.py
@@ -42,35 +42,3 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 )
-
-#
-# # Where the magic happens:
-# setup(
-#     name=NAME,
-#     version=about['__version__'],
-#     description=DESCRIPTION,
-#     long_description=long_description,
-#     long_description_content_type='text/markdown',
-#     author=AUTHOR,
-#     author_email=EMAIL,
-#     python_requires=REQUIRES_PYTHON,
-#     url=URL,
-#     packages=find_packages(exclude=('tests',)),
-#     package_data={'regression_model': ['VERSION']},
-#     install_requires=list_reqs(),
-#     extras_require={},
-#     include_package_data=True,
-#     license='MIT',
-#     classifiers=[
-#         # Trove classifiers
-#         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-#         'License :: OSI Approved :: MIT License',
-#         'Programming Language :: Python',
-#         'Programming Language :: Python :: 3',
-#         'Programming Language :: Python :: 3.6',
-#         'Programming Language :: Python :: 3.7',
-#         'Programming Language :: Python :: 3.8',
-#         'Programming Language :: Python :: Implementation :: CPython',
-#         'Programming Language :: Python :: Implementation :: PyPy'
-#     ],
-# )
